fc731e1725adfaffaec3b67409d4b3e6a4c4fd49
jocoso <joshua.collado022@gmail.com>
2022-08-20 09:25:58 -0400

    Cat speaks

--- tests/pet_displayer.py
+++ tests/pet_displayer.py
@@ -1,45 +1,58 @@
-import pyautogui
 import random
 import threading
 import tkinter as tk
-from PIL import Image, ImageTk
+import pyttsx3
+# from PIL import Image, ImageTk
 import os
 
 class Pet(tk.Tk):
     def __init__(self, catimagepath, frame_num):
         tk.Tk.__init__(self)
 
+        #initializing variables
+        self.engine = pyttsx3.init()
         self.cycle = 0
         self.check = 0
         self.idle_num =[i + 1 for i in range(frame_num)] # self.idle_num = [1, 2, 3, ...]
         self.event_number = 0
         self.frame = None
-        self.idle = [tk.PhotoImage(file=catimagepath, format = 'gif -index %i' %(i)) for i in range(frame_num)]#idle gif
         self.talking = False
 
+        # Images
+        self.idle = [tk.PhotoImage(file=catimagepath, format = 'gif -index %i' %(i)) for i in range(frame_num)]#idle gif
+
+        # Prepping
         self.set_window_config()
         self.set_menu()
         self.set_labels()
+        self.set_inputs()
 
     # ====== Initialization ======
     def set_labels(self):
         # Floating Window
-        self.label.pack(side="right", fill="both", expand=True)
+        self.label = tk.Label(self,bd=0,bg='black')
+        self.label.pack(side="left", fill="both", expand=True)
+        
+        self.label.pack()
+
+    def say_hello(self):
+        self.engine.say('Hola')
+        self.engine.runAndWait()
+
+    def set_inputs(self):
+        self.bind("<Button-3>", self.open_action_menu)
         self.label.bind("<ButtonPress-1>", self.start_move)
         self.label.bind("<ButtonRelease-1>", self.stop_move)
         self.label.bind("<B1-Motion>", self.do_move)
 
-        self.bubblelabel = tk.Label(self, bg='red', width=60, height=120)
-        
-        self.label.pack()
-        self.bubblelabel.pack()
-
     def set_menu(self):
         self.rclickmenu = tk.Menu(self, tearoff=0)
+        self.rclickmenu.add_command(label='Hello', command=self.say_hello)
         self.rclickmenu.add_command(label='Log Off', command=self.exit_program)
-        self.bind("<Button-3>", self.open_action_menu)
 
     def exit_program(self):
+        self.engine.say('Adios')
+        self.engine.runAndWait()
         self.destroy()
         exit()
     
@@ -52,7 +65,6 @@ class Pet(tk.Tk):
     # Configures window specifications
     def set_window_config(self):
         self.config(highlightbackground='black')
-        self.label = tk.Label(self,bd=0,bg='black')
         self.overrideredirect(True)
         self.wm_attributes('-transparentcolor','black')
 
@@ -74,7 +86,7 @@ class Pet(tk.Tk):
             self.frame = self.idle[self.cycle]
             self.gif_work()
             
-        self.geometry('300x300+'+str(self.winfo_x())+'+'+str(self.winfo_y()))
+        self.geometry('320x300+'+str(self.winfo_x())+'+'+str(self.winfo_y()))
         self.label.configure(image=self.frame)
         self.after(1, self.event)
 
@@ -97,17 +109,7 @@ class Pet(tk.Tk):
     # ====== Public Functions ======
     def add_action(self, name, action):
         self.rclickmenu.add_command(label=name, command=action)
-    
-    def say(self, bubbleimage, text):
-        print(text)
-        self.talking = True
-        self.geometry("360x300")
-        image = Image.open(bubbleimage)
-        img = image.resize((10, 10))
-        speech = ImageTk.PhotoImage(img)
-        self.bubblelabel.configure(image=speech)
         
-
     def run(self):
         #loop the program
         self.after(1,self.update)
@@ -115,19 +117,10 @@ class Pet(tk.Tk):
 
 
 
-
 impath = os.path.realpath("../resources") + '\\'
-catimagepath = impath + 'cat_idle.gif'
-bubbleimagepath = impath + 'speech_bubble.png'
+catimagepath = impath + 'cat_idle_cropped.gif'
 
 pet = Pet(catimagepath, 5)
-
-def print_hello():
-    pet.say(bubbleimagepath, 'hello')
-
-pet.add_action('Hello', action=print_hello)
-pet.say( bubbleimagepath, 'hello')
-
 pet.run()
 
 
